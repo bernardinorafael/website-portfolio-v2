ecdf03bae8606049a47eac3e3ba65470ea671cb8
Rafael <rafaelferreirab2@gmail.com>
2022-11-12 23:38:08 -0300

    :lipstick: Estilização da navbar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,27 @@
-import { Container } from "./styles"
+import Image from "next/image"
+import { ButtonToggleTheme, Container } from "./styles"
+import * as Icon from "phosphor-react"
+import Link from "next/link"
 
 export function Header() {
   return (
     <Container>
-      <h1>Hello world</h1>
+      <div>
+        <Image src="/images/imagem-main-perfil.png" height={36} width={36} alt="" />
+
+        <nav>
+          <Link href="#">Sobre mim</Link>
+          <Link href="#">Apps</Link>
+
+          <ButtonToggleTheme>
+            <Icon.User size={20} weight="duotone" />
+          </ButtonToggleTheme>
+        </nav>
+
+        <ButtonToggleTheme>
+          <Icon.Sun size={20} weight="duotone" />
+        </ButtonToggleTheme>
+      </div>
     </Container>
   )
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,56 @@
 import styled from "styled-components"
 
-export const Container = styled.div``
+export const Container = styled.header`
+  background: ${({ theme }) => theme.colors.gray[800]};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray[700]};
+  height: 4rem;
+  inset: 0;
+  position: fixed;
+  z-index: 100;
+
+  div {
+    align-items: center;
+    display: flex;
+    height: 4rem;
+    justify-content: space-between;
+    margin: 0 auto;
+    max-width: 720px;
+    padding: 0 2rem;
+    user-select: none;
+
+    nav {
+      display: flex;
+      gap: 1rem;
+
+      a {
+        border-radius: ${({ theme }) => theme.radii.sm};
+        color: ${({ theme }) => theme.colors.gray[300]};
+        height: 2.25rem;
+        line-height: 2.25rem;
+        padding: 0 0.5rem;
+      }
+    }
+  }
+`
+
+export const ButtonToggleTheme = styled.button`
+	align-items: center;
+	display: flex;
+	justify-content: center;
+  background: ${({ theme }) => theme.colors.gray[700]};
+  border-radius: ${({ theme }) => theme.radii.full};
+  border: none;
+  height: 2.25rem;
+  line-height: 0;
+  outline: none;
+  width: 2.25rem;
+
+  &:hover {
+    filter: brightness(1.1);
+    transition: filter 0.3s;
+  }
+
+  svg {
+    color: ${({ theme }) => theme.colors.gray[300]};
+  }
+`
