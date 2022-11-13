bf4498242d42eca7113fca7f8b7f7537bf08a6c4
Rafael <rafaelferreirab2@gmail.com>
2022-11-13 17:20:17 -0300

    :lipstick: Estilização da página Home

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -25,7 +25,15 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,400;0,500;0,600;1,300&family=IBM+Plex+Sans:wght@300;400;500;600&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="shortcut icon" href="/favicon.png" />
+        </Head>
         <body>
           <Main />
           <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,86 @@
+import Head from "next/head"
+import Image from "next/image"
+import * as Icon from "phosphor-react"
+import Link from "next/link"
+import Title from "../components/Title"
+import { Container, Project, ProjectsContainer } from "../css/pages/home"
+
 function Home() {
-  return <div></div>
+  return (
+    <>
+      <Head>
+        <title>Home | Rafael Bernardino</title>
+      </Head>
+
+      <Container>
+        <section>
+          <Title>Hello, I&apos;m Rafael 👋</Title>
+
+          <p>
+            Desenvolvedor web, nascido em Criciúma, Santa Catarina, região sul do país
+            <br />e atualmente morando em Sobral, Ceará. Meu contato com a tecnologia veio
+            desde cedo, minha família sempre foi envolvida com a informática. Já estive na área
+            têxtil, um segmento onde a indústria é bem estabelecida em Santa Catarina, porém no
+            decorrer do tempo, me reencontrei na área da tecnologia.
+          </p>
+        </section>
+
+        <section>
+          <Title>Meus projetos</Title>
+
+          <ProjectsContainer>
+            <Project>
+              <div>
+                <Image height={180} width={180} src="/images/mockflow.png" alt="" />
+              </div>
+
+              <footer>
+                <strong>Shopping</strong>
+
+                <div>
+                  <span>TypeScript</span>
+                  <span>Next.js</span>
+                </div>
+              </footer>
+            </Project>
+            <Project>
+              <div>
+                <Image height={180} width={180} src="/images/mockflow.png" alt="" />
+              </div>
+
+              <footer>
+                <strong>Shopping</strong>
+
+                <div>
+                  <span>TypeScript</span>
+                  <span>Next.js</span>
+                </div>
+              </footer>
+            </Project>
+            <Project>
+              <div>
+                <Image height={180} width={180} src="/images/mockflow.png" alt="" />
+              </div>
+
+              <footer>
+                <strong>Shopping</strong>
+
+                <div>
+                  <span>TypeScript</span>
+                  <span>Next.js</span>
+                </div>
+              </footer>
+            </Project>
+          </ProjectsContainer>
+
+          <Link href="#">
+            Ver mais projetos
+            <Icon.ArrowRight />
+          </Link>
+        </section>
+      </Container>
+    </>
+  )
 }
 
 export default Home
